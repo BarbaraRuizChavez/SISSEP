9488f6e55b14418aeaf9904a0c808461ebd1d02a
albakugo <ayalavillalonalondra@gmail.com>
2026-04-06 17:19:47 -0600

    Borrar primera versión del src

--- frontend/src/components/PerfilUsuario.tsx
+++
@@ -1,8 +0,0 @@
-export default function PerfilUsuario() {
-  return (
-    <div className="perfil-usuario">
-      <h2>Bienvenido, Usuario</h2>
-      <p>Panel de gestión de documentos</p>
-    </div>
-  );
-}
