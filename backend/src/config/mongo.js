cc01d272b3a830ca45f16d6e8671dcd347c85cc6
CarlosSantiagoChipahua <s23120236@alumnos.itsur.edu.mx>
2026-04-23 21:10:55 -0600

    Delete backend directory
    Eliminar el backend, para posteriormente subir los archivos correctos

--- backend/src/config/mongo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const connectMongo = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB conectado");
-};
-
-module.exports = connectMongo;
--- backend/test.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/api/users", (req, res) => {
-  console.log("Petición GET /api/users recibida");
-  res.json([{ name: "Test User" }]);
-});
-
-app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
